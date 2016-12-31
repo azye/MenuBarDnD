0a1a37d2669597d0b4c7ed0ce88561676a50b247
Alex Ye <alexzye1@gmail.com>
2016-12-31 12:33:08 -0800

    Initial Commit

---
+++ MenuBarDnD/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MenuBarDnD
+//
+//  Created by Alex Z.Y. Ye on 12/31/16.
+//  Copyright © 2016 Alex Z.Y. Ye. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
