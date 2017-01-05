88b2b102baf8d38d91c0467dc7067684040c0f46
Alex Ye <alexzye1@gmail.com>
2017-01-05 00:03:06 -0800

    Remove delay before running script

--- MenuBarDnD/MenuBarDnD/AppDelegate.swift
+++ MenuBarDnD/MenuBarDnD/AppDelegate.swift
@@ -45,16 +45,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func midnightCheck() {
-        if dndIsActive {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
-                let defaults = UserDefaults(suiteName: "com.apple.notificationcenterui")
-                let state = defaults?.bool(forKey: "doNotDisturb") ?? false
-                
-                if state != self.dndIsActive {
-                    self.runScript()
-                }
-            })
-        }
+            let defaults = UserDefaults(suiteName: "com.apple.notificationcenterui")
+            let state = defaults?.bool(forKey: "doNotDisturb") ?? false
+            
+            if state != self.dndIsActive {
+                self.runScript()
+            }
     }
  
     func quitApplication() {
